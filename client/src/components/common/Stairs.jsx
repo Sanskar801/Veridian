81313d250b63afc1bb8bd2047ef2dd6a8a913825
Sanskar Jain <sanskarj257@gmail.com>
2025-10-29 11:17:37 +0530

    Responsive page change animation

--- client/src/components/common/Stairs.jsx
+++ client/src/components/common/Stairs.jsx
@@ -11,46 +11,55 @@ const Stairs = ({ children }) => {
 
     useGSAP(() => {
 
-        const tl = gsap.timeline();
+        const mm = gsap.matchMedia();
 
-        tl.to(stairParentRef.current, {
-            display: "block",
-        })
-        tl.from('.stair', {
-            height: 0,
-            stagger: {
-                amount: -0.25,
-            },
-        })
-        tl.to('.stair', {
-            y: '100%',
-            stagger: {
-                amount: -0.25,
-            }
-        })
-        tl.to(stairParentRef.current, {
-            display: "none",
-        })
-        tl.to('.stair', {
-            y: "0%",
-        })
+        mm.add({
+            isMobile: "(max-width: 640px)",
+            isTablet: "(min-width: 641px) and (max-width: 1024px)",
+            isDesktop: "(min-width: 1025px)"
+        }, (context) => {
+            const { isMobile, isTablet } = context.conditions;
 
-        gsap.from(pageRef.current, {
-            opacity: 0,
-            delay: 1.5,
-        })
+            const staggerAmount = isMobile ? -0.15 : (isTablet ? -0.2 : -0.25);
+            const pageDelay = isMobile ? 1.0 : (isTablet ? 1.3 : 1.5);
+
+            const tl = gsap.timeline();
+
+            tl.set(stairParentRef.current, { display: "block" });
+            tl.from('.stair', {
+                height: 0,
+                stagger: { amount: staggerAmount },
+            });
+            tl.to('.stair', {
+                y: '100%',
+                stagger: { amount: staggerAmount },
+            });
+            tl.set(stairParentRef.current, { display: "none" });
+            tl.set('.stair', { y: '0%' });
+
+            gsap.from(pageRef.current, {
+                opacity: 0,
+                delay: pageDelay,
+            });
+
+            return () => {
+                tl.kill();
+            };
+        });
+
+        return () => mm.revert();
 
     }, [currentPath])
 
     return (
         <>
-            <div ref={stairParentRef} className='h-screen w-full fixed z-10 top-0'>
+            <div ref={stairParentRef} className='h-screen w-full fixed z-10 top-0 left-0 pointer-events-none' style={{ height: '100svh' }}>
                 <div className='h-full w-full flex'>
-                    <div className='stair h-full w-1/5 bg-black'></div>
-                    <div className='stair h-full w-1/5 bg-black'></div>
-                    <div className='stair h-full w-1/5 bg-black'></div>
-                    <div className='stair h-full w-1/5 bg-black'></div>
-                    <div className='stair h-full w-1/5 bg-black'></div>
+                    <div className='stair h-full w-1/5 bg-black transform-gpu'></div>
+                    <div className='stair h-full w-1/5 bg-black transform-gpu'></div>
+                    <div className='stair h-full w-1/5 bg-black transform-gpu'></div>
+                    <div className='stair h-full w-1/5 bg-black transform-gpu'></div>
+                    <div className='stair h-full w-1/5 bg-black transform-gpu'></div>
                 </div>
             </div>
             <div ref={pageRef}>
